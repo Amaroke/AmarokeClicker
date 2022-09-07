b83c87b3d5ef2450458b8bff7b3c431c1cb618f9
Amaroke <hugo.amaroke@gmail.com>
2022-09-07 21:17:54 +0200

    Modification de la vitesse de production, et de la gestion des éléments graphiques.

--- app/src/main/java/com/example/amarokeclicker/MainActivity.kt
+++ app/src/main/java/com/example/amarokeclicker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.amarokeclicker
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -8,49 +9,43 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var progressBar: ProgressBar
+    private lateinit var viewCoins: TextView
+    private lateinit var production1: Button
     private var progressBarStatus = 0
-    private var dummy = 0
     private var coins = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // get the references from layout file
-        val btnStartProgress = this.findViewById<Button>(R.id.button_production1)
-        val progressBar = this.findViewById<ProgressBar>(R.id.progressBar_production1)
-        val viewCoins = this.findViewById<TextView>(R.id.textView_coins)
+        progressBar = this.findViewById(R.id.progressBar_production1)
+        viewCoins = this.findViewById(R.id.textView_coins)
+        production1 = this.findViewById(R.id.button_production1)
         viewCoins.text = coins.toString()
+    }
 
-        // when button is clicked, start the task
-        btnStartProgress.setOnClickListener { v ->
+    fun onClickButton1(v: View) {
+        production1.isClickable = false
+        Thread {
 
-            // task is run on a thread
-            Thread {
-                // dummy thread mimicking some operation whose progress can be tracked
-                while (progressBarStatus < 100) {
-                    // performing some dummy operation
-                    try {
-                        dummy += 25
-                        Thread.sleep(1000)
-                    } catch (e: InterruptedException) {
-                        e.printStackTrace()
-                    }
+            while (progressBarStatus < 100) {
 
-                    // tracking progress
-                    progressBarStatus = dummy
-
-                    // Updating the progress bar
-                    progressBar.progress = progressBarStatus
+                try {
+                    progressBarStatus += 1
+                    Thread.sleep(10)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
                 }
-                progressBar.progress=0
-                progressBarStatus = 0
-                dummy=0
-                coins+=1
-                viewCoins.text = coins.toString()
+                progressBar.progress = progressBarStatus
 
+            }
 
-            }.start()
+            progressBarStatus = 0
+            progressBar.progress = progressBarStatus
+            coins += 1
+            viewCoins.text = coins.toString()
+            production1.isClickable = true
 
-        }
+        }.start()
     }
 }
