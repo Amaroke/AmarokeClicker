9effcf74921ab72f9ba8782fda2885cf7b9cee99
Amaroke <hugo.amaroke@gmail.com>
2022-09-15 21:47:44 +0200

    Première liaison de l'UI au code.

--- app/src/main/java/com/example/amarokeclicker/MainActivity.kt
+++ app/src/main/java/com/example/amarokeclicker/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -12,11 +13,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var progressBar: ProgressBar
     private lateinit var viewCoins: TextView
-    private lateinit var production1: Button
+    private lateinit var production1: ImageButton
     private lateinit var buypoulet: Button
     private var progressBarStatus = 0
     private var coins = 0
     private var chicken = Chicken()
+    private lateinit var viewChicken: View
+    private lateinit var viewHeader: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,52 +28,46 @@ class MainActivity : AppCompatActivity() {
         this.supportActionBar?.hide()
         //On définit le contenu de la vue APRES les instructions précédentes pour éviter un crash
         this.setContentView(R.layout.activity_main)
-        //progressBar = this.findViewById(R.id.progressBar_production1)
-        //viewCoins = this.findViewById(R.id.textView_coins)
-        //production1 = this.findViewById(R.id.button_production1)
-        //buypoulet = this.findViewById(R.id.button_buy1)
-        //refreshView()
-    }
-/*
-    fun onClickButton1(v: View) {
-        production1.isClickable = false
-        Thread {
 
-            while (progressBarStatus < 100) {
+        viewChicken = this.findViewById(R.id.chicken)
+        viewHeader = this.findViewById(R.id.header)
 
-                try {
-                    progressBarStatus += 1
-                    Thread.sleep(chicken.actualProductionTime.toLong())
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
-                }
-                progressBar.progress = progressBarStatus
+        progressBar = viewChicken.findViewById(R.id.progressBarProduction)
+        viewCoins = viewHeader.findViewById(R.id.textViewMoney)
 
-            }
+       production1 = viewChicken.findViewById(R.id.imageButtonProduction)
+       production1.setOnClickListener {
 
-            progressBarStatus = 0
-            progressBar.progress = progressBarStatus
-            coins += chicken.actualProduction
-            refreshView()
-            production1.isClickable = true
+           production1.isClickable = false
+           Thread {
 
-        }.start()
-    }
+               while (progressBarStatus < 100) {
 
-    fun onclickbuypoulet(v : View) {
-        if(coins>=chicken.actualCost) {
-            coins-=chicken.actualCost
-            chicken.actualCost+=2
-            chicken.actualProduction+=1
-            chicken.numberPossessed+=1
-            refreshView()
-        }
+                   try {
+                       progressBarStatus += 1
+                       Thread.sleep(chicken.actualProductionTime.toLong())
+                   } catch (e: InterruptedException) {
+                       e.printStackTrace()
+                   }
+                   progressBar.progress = progressBarStatus
 
+               }
+
+               progressBarStatus = 0
+               progressBar.progress = progressBarStatus
+               coins += chicken.actualProduction
+               refreshView()
+               production1.isClickable = true
+
+           }.start()
+       }
+       buypoulet = viewChicken.findViewById(R.id.buttonUpgradeProduction)
+
+
+       refreshView()
     }
 
     private fun refreshView() {
         viewCoins.text = coins.toString()
-        this.findViewById<TextView>(R.id.textView_cost1).text= "Coût :" + chicken.actualCost.toString()
-        this.findViewById<TextView>(R.id.textView_get1).text = "Possédé :" + chicken.numberPossessed.toString() + "\n(+ "+chicken.actualProduction+" coins à chaque production"
-    }*/
+    }
 }
